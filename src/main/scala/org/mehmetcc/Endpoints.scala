8154a0353669822da0e636176fe0848a392ab288
mehmetcc <mehmetccm@gmail.com>
2023-03-09 23:50:56 +0300

    decouple models from endpoints.scala

--- src/main/scala/org/mehmetcc/Endpoints.scala
+++ src/main/scala/org/mehmetcc/Endpoints.scala
@@ -11,47 +11,8 @@ import sttp.tapir.server.metrics.prometheus.PrometheusMetrics
 import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import sttp.tapir.ztapir.ZServerEndpoint
 import zio.Task
-import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
 
 import scala.language.postfixOps
-
-case class PostAccelerationOnceRequest(x: Double, y: Double, z: Double)
-
-object PostAccelerationOnceRequest {
-  implicit val postAccelerationOnceRequestEncoder: JsonEncoder[PostAccelerationOnceRequest] =
-    DeriveJsonEncoder.gen[PostAccelerationOnceRequest]
-
-  implicit val postAccelerationOnceRequestEncoderDecoder: JsonDecoder[PostAccelerationOnceRequest] =
-    DeriveJsonDecoder.gen[PostAccelerationOnceRequest]
-}
-
-case class PostAccelerationManyRequest(accelerations: List[PostAccelerationOnceRequest])
-
-object PostAccelerationManyRequest {
-  implicit val postAccelerationManyRequestEncoder: JsonEncoder[PostAccelerationManyRequest] =
-    DeriveJsonEncoder.gen[PostAccelerationManyRequest]
-
-  implicit val postAccelerationManyRequestDecoder: JsonDecoder[PostAccelerationManyRequest] =
-    DeriveJsonDecoder.gen[PostAccelerationManyRequest]
-}
-
-case class PostAccelerationResponse(isSuccess: Boolean)
-
-object PostAccelerationResponse {
-  implicit val postAccelerationResponseEncoder: JsonEncoder[PostAccelerationResponse] =
-    DeriveJsonEncoder.gen[PostAccelerationResponse]
-
-  implicit val postAccelerationResponseDecoder: JsonDecoder[PostAccelerationResponse] =
-    DeriveJsonDecoder.gen[PostAccelerationResponse]
-}
-
-case class ErrorMessage(message: String)
-
-object ErrorMessage {
-  implicit val errorMessageEncoder: JsonEncoder[ErrorMessage] = DeriveJsonEncoder.gen[ErrorMessage]
-
-  implicit val errorMessafeDecoder: JsonDecoder[ErrorMessage] = DeriveJsonDecoder.gen[ErrorMessage]
-}
 
 object Endpoints {
   val postAccelerationOnceServerEndpoint: ZServerEndpoint[Any, Any] =
